30d643507826b2bb9450ca4a91db65159755b69e
Chris Harris <cjhdev@outlook.com>
2021-10-02 20:34:14 -0500

    Adding twitter feed

---
+++ NewWorldData/Controllers/TwitterController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewWorldData.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NewWorldData.Controllers
+{
+    public class TwitterController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public IEnumerable<TwitterModel> Tweets { get; set; }
+        const string TWITTER_BASE_URL = "https://newworldfans.com/api/v1/dev_tracker?page=1&source=twitter";
+        public TwitterController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var message = new HttpRequestMessage();
+            message.Method = HttpMethod.Get;
+            message.RequestUri = new Uri($"{TWITTER_BASE_URL}");
+            message.Headers.Add("Accept", "application/json");
+
+            var client = _clientFactory.CreateClient();
+            var response = await client.SendAsync(message);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                Tweets = await JsonSerializer.DeserializeAsync<IEnumerable<TwitterModel>>(responseStream);
+            }
+
+            return View(Tweets);
+        }
+    }
+}
---
+++ NewWorldData/Models/TwitterModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewWorldData.Models
+{
+    public class TwitterModel
+    {
+        [Display(Name = "ID")]
+        [Key]
+        public int id { get; set; }
+        [Display(Name = "Title")]
+        public string title { get; set; }
+        [Display(Name = "Content")]
+        public string content { get; set; }
+        public string source_url { get; set; }
+        public string posted_at { get; set; }
+        public string developer_name { get; set; }
+    }
+}
